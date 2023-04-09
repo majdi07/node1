519d2ae4726fb33881cff6cf6783c55efcd66999
majdi07 <majd92@live.fr>
2023-04-09 07:16:24 +0200

    hello

---
+++ server.js
@@ -0,0 +1,78 @@
+const http = require("http");
+const hello = require("./hello-word")
+const fs = require("fs");
+const nodemailer = require('nodemailer')
+
+var generator = require('generate-password');
+
+var password = generator.generate({
+    length: 10,
+    numbers: true
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
+
+let data = "Hello Node";
+
+fs.writeFile("welcome.txt", data, (err) => {
+    if (err)
+        console.log(err);
+    else {
+        console.log("File written successfully\n");
+        console.log("The written has the following contents:");
+        console.log(fs.readFileSync("welcome.txt", "utf8"));
+    }
+});
+
+
+
+
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html;charset=utf8" })
+    res.write('<h1>Hello Node!!!!</h1>\n')
+    res.end()
+})
+
+
+
+
+
+
+console.log("connected");
+server.listen(3000)
